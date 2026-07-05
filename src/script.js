27883720dca251d662c75ab0519b62f0c12f3daf
Caleb Lira <contatocaleb2010@gmail.com>
2026-07-05 00:55:19 -0300

    section skills feita

--- src/script.js
+++ src/script.js
@@ -46,165 +46,75 @@ document.addEventListener("mousemove", (e) => {
   glow.style.top = e.clientY - 80 + "px";
 });
 
-const groupButtons = document.querySelectorAll("[data-tab]");
-const grid = document.getElementById("skills-grid");
+const slider = document.getElementById("frontSlider");
+const prevBtn = document.getElementById("prevFront");
+const nextBtn = document.getElementById("nextFront");
 
-function criarCard(titulo,logo){
-  const card = document.createElement("div");
+function updateButtons() {
+    if (slider.scrollLeft <= 0) {
+        prevBtn.classList.add("opacity-0", "pointer-events-none");
+    } else {
+        prevBtn.classList.remove("opacity-0", "pointer-events-none");
+    }
 
-  card.classList.add(
-    "p-6",
-    "min-h-[100px]",
-    "rounded-lg",
-    "bg-gradient-to-br",
-    "from-slate-900",
-    "to-slate-800",
-    "border",
-    "border-slate-700/30",
-    "transition-all",
-    "hover:-translate-y-2",
-    "hover:scale-[1.03]",
-    "duration-300",
-    "flex",
-    "flex-col",
-    "justify-center",
-    "items-center",
-    "text-center",
-    "hover:border-secondary/50",
-  );
-
-  card.innerHTML = `
-  <div class="w-[70px] h-[70px] mb-5">
-   <img src="${logo}" alt="${titulo} logo" class="w-full h-full object-contain">
-  </div>
-
-    <h3 class="text-xl font-bold mb-2 text-center">${titulo}</h3>
-  `;
-
-  return card;
+    if (
+        slider.scrollLeft + slider.clientWidth >=
+        slider.scrollWidth - 5
+    ) {
+        nextBtn.classList.add("opacity-0", "pointer-events-none");
+    } else {
+        nextBtn.classList.remove("opacity-0", "pointer-events-none");
+    }
 }
-
-function renderizar(tab){
-
-  grid.classList.add(
-    "opacity-0",
-    "translate-y-2"
-  );
-
-  setTimeout(() => {
-
-    grid.innerHTML = "";
-
-    if(tab === "languages"){
-
-      grid.appendChild(
-        criarCard(
-          "JavaScript",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "HTML",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "CSS",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "PHP",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
-        )
-      );
-
-    }
-
-    else if(tab === "frameworks"){
-
-      grid.appendChild(
-        criarCard(
-          "React",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "Tailwind CSS",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "Bootstrap",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-        )
-      );
-
-    }
-
-    else if(tab === "tools"){
-
-      grid.appendChild(
-        criarCard(
-          "Git",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "GitHub",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "NPM",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original.svg"
-        )
-      );
-
-      grid.appendChild(
-        criarCard(
-          "NodeJs",
-          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-        )
-      );
-
-    }
-
-    requestAnimationFrame(() => {
-
-      grid.classList.remove(
-        "opacity-0",
-        "translate-y-2"
-      );
-
-      grid.classList.add(
-        "opacity-100",
-        "translate-y-0"
-      );
-
+nextBtn.addEventListener("click", () => {
+    slider.scrollBy({
+        left: 220,
+        behavior: "smooth",
     });
-
-  }, 250);
-}
-
-groupButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    renderizar(button.dataset.tab);
-  });
 });
+prevBtn.addEventListener("click", () => {
+    slider.scrollBy({
+        left: -220,
+        behavior: "smooth",
+    });
+});
+slider.addEventListener("scroll", updateButtons);
+updateButtons();
 
-renderizar("languages");
+
+const sliderr = document.getElementById("develoupSlider");
+const previBtn = document.getElementById("previousFront");
+const nextiBtn = document.getElementById("nextiousFront");
+
+
+function updateeButtons() {
+    if (sliderr.scrollLeft <= 0) {
+        previBtn.classList.add("opacity-0", "pointer-events-none");
+    } else {
+        previBtn.classList.remove("opacity-0", "pointer-events-none");
+    }
+
+    if (
+        sliderr.scrollLeft + sliderr.clientWidth >=
+        sliderr.scrollWidth - 5
+    ) {
+        nextiBtn.classList.add("opacity-0", "pointer-events-none");
+    } else {
+        nextiBtn.classList.remove("opacity-0", "pointer-events-none");
+    }
+}
+nextiBtn.addEventListener("click", () => {
+    sliderr.scrollBy({
+        left: 220,
+        behavior: "smooth",
+    });
+});
+previBtn.addEventListener("click", () => {
+    sliderr.scrollBy({
+        left: -220,
+        behavior: "smooth",
+    });
+});
+sliderr.addEventListener("scroll", updateeButtons);
+updateeButtons();
+
